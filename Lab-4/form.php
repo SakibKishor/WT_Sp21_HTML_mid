c607ce44701702b0a8baae3cc9da7684bbf65d6c
Sakib Hasan <sakishor.h@gmail.com>
2021-03-14 22:40:50 +0600

    updated

--- Lab-4/form.php
+++ Lab-4/form.php
@@ -10,12 +10,20 @@
 			$err_password="";
 			$cpass="";
 			$err_cpass="";
+			$email="";
+			$err_email="";
 			$err_gender="";
 			$biodata="";
 			$err_bio="";
 			$number="";
 			$err_number="";
 			$code="";
+			$saddress="";
+			$city="";
+			$postal="";
+			$err_address="";
+			
+			$hasError=false;
 			
 			
 		
@@ -24,14 +32,16 @@
 				if (empty($_POST["name"]))
 				{
 					$err_name="*Name Required";
+					$hasError=true;
 				}
 				else if(strlen($_POST["name"])<2 )
 				{
 					$err_name="*Name should be more than one characters";
+					$hasError=true;
 				}
 				else
 				{
-					$name= $_POST["name"];
+					$name=htmlspecialchars( $_POST["name"]);
 				}
 				
 				
@@ -39,50 +49,58 @@
 				if (empty($_POST["uname"]))
 				{
 					$err_uname="*Username Required";
+					$hasError=true;
 				}
 				else if(strlen($_POST["uname"])<6 )
 				{
 					$err_uname="*Username should be at least 6 characters";
+					$hasError=true;
 				}
 				else
 				{
-					$uname= $_POST["uname"];
+					$uname=htmlspecialchars( $_POST["uname"]);
 				}
 				
 				
 				if (empty($_POST["pass"]))
 				{
 					$err_password="*Password Required";
+					$hasError=true;
 				}
 				
 				else if (ctype_alpha($_POST["pass"]))
 				{
 					$err_password="*Password should contain at least one numeric character";
+					$hasError=true;
 				}
 				
 				else if (ctype_lower($_POST["pass"]))
 				{
 					$err_password="*Password should contain at least one uppercase character";
+					$hasError=true;
 				}
 				
 				else if (ctype_upper($_POST["pass"]))
 				{
 					$err_password="*Password should contain at least one lowercase character";
+					$hasError=true;
 				}
 				
 				else if (ctype_alnum ($_POST["pass"]))
 				{
 					$err_password="*Password should contain at least one special character";
+					$hasError=true;
 				}
 				
 				else if (strlen($_POST["pass"])<8)
 				{
 					$err_password="*Password should contain at least 8 characters";
+					$hasError=true;
 				}
 				
 				else
 				{
-					$password=$_POST["pass"];
+					$password=htmlspecialchars($_POST["pass"]);
 				}
 				
 				
@@ -90,21 +108,43 @@
 				if ($_POST["pass"]!=$_POST["cpass"])
 				{
 					$err_cpass="*Password does not match";
+					$hasError=true;
 				}
 				else
 				{
-					$cpass=$_POST["cpass"];
+					$cpass=htmlspecialchars($_POST["cpass"]);
 				}
 				
+				if(empty($_POST["email"]))
+				{
+					$err_email="*Email Required";
+					$hasError=true;
+				}
+				else{
+					$email=htmlspecialchars($_POST["email"]);
+				}
 				
+				if(empty($_POST["city"]) || empty($_POST["postal"]) || empty($_POST["street"]))
+				{
+					$err_address="*Please fill out all the address";
+					$hasError=true;
+				}
 				
+				else
+				{
+					
+				}
+			
+			
+			
 				if(!ctype_digit($_POST["number"]))
 				{
 					$err_number="*Phone no should be all numbers";
+					$hasError=true;
 				}
 				else
 				{
-					$number=$_POST["number"];
+					$number=htmlspecialchars($_POST["number"]);
 				}
 				
 				
@@ -112,10 +152,11 @@
 				if(!ctype_digit($_POST["code"]))
 				{
 					$err_number="*Phone no should be all numbers";
+					$hasError=true;
 				}
 				else
 				{
-					$code=$_POST["code"];
+					$code=htmlspecialchars($_POST["code"]);
 				}
 				
 				
@@ -132,25 +173,31 @@
 				if (empty($_POST["bio"]))
 				{
 					$err_bio="*Biodata required";
+					$hasError=true;
 				}
 				else if(strlen($_POST["bio"])<10 )
 				{
 					$err_bio="*Biodata should be more than 10 words";
+					$hasError=true;
 				}
 				else
 				{
-					$biodata= $_POST["bio"];
+					$biodata= htmlspecialchars($_POST["bio"]);
 				}
 				
+				if(!$hasError)
+				{
+					echo "Username:$uname";
+				}
 				
 		
 			}
 		?>
 	
 	<fieldset>
-		<legend><h1>Club Member Regestration</h1></legend>
+		<legend align= "center"><h1 >Club Member Regestration</h1></legend>
 		<form action="" method= "post">
-			<table>
+			<table align= "center">
 				<tr>
 					<td> <span>Name</span> </td>
 					<td> :<input type = "text" value="<?php echo $name;?>"  name= "name"> 
@@ -176,7 +223,8 @@
 				
 				<tr>
 					<td> <span>Email</span> </td>
-					<td> :<input type = "text"  name= "email"> </td>
+					<td> :<input type = "text" value= "<?php echo $email;?>" name= "email">
+					<span> <?php echo $err_email; ?> </span> </td>
 					
 				</tr>
 				
@@ -194,8 +242,8 @@
 					<td> :<input type = "text" placeholder="Street Address"  name= "street"> <br>
 						:<input size="1" type = "text" placeholder="City"  name= "city"> 
 						-<input size="12" type = "text" placeholder="State"  name= "state"> <br>
-						:<input type = "text" placeholder="Postal/Zip code"  name= "street"> </td>
-					
+						:<input type = "text" placeholder="Postal/Zip code"  name= "postal"> 
+						<span> <?php echo $err_address; ?> </span> </td> 
 				</tr>
 				
 				<tr>
